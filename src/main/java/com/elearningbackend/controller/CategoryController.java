1302eb0f4110865d4955bb3dc908d77ab6322720
dohalong <dohalong1993@gmail.com>
2017-12-18 21:20:05 +0700

    Add authenticate for subcate and cate

--- src/main/java/com/elearningbackend/controller/CategoryController.java
+++ src/main/java/com/elearningbackend/controller/CategoryController.java
@@ -10,16 +10,18 @@ import com.elearningbackend.utility.ResultCodes;
 import com.elearningbackend.utility.SortingConstants;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin
-public class CategoryController {
+public class CategoryController extends BaseController {
     @Autowired
     @Qualifier("categoryService")
     private IAbstractService<CategoryDto, String> abstractService;
 
     @GetMapping("/categories")
+    @PreAuthorize(Constants.PRE_AUTHENTICATED)
     public Pager<CategoryDto> loadAll(
             @RequestParam(value = "page", defaultValue = Constants.CURRENT_PAGE_DEFAULT_STRING_VALUE) int page,
             @RequestParam(value = "limit", defaultValue = Constants.NO_OF_ROWS_DEFAULT_STRING_VALUE) int noOfRowInPage,
@@ -29,6 +31,7 @@ public class CategoryController {
     }
 
     @GetMapping("/categories/{key}")
+    @PreAuthorize(Constants.PRE_AUTHENTICATED)
     public Result<CategoryDto> getByKey(@PathVariable("key") String key){
         try{
             CategoryDto categoryDto = abstractService.getOneByKey(key);
